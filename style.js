47b590e51bd9dcf5894476f413100cb4b727b251
Umair <cyumair@hotmail.com>
2021-02-03 13:37:13 +0500

    replacing old javascript syntax with ES

--- style.js
+++ style.js
@@ -1,9 +1,8 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-console.log(color2)
-function background(){
+const css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+ background = () => {
 	body.style.background =
 	"linear-gradient(to right ," + color1.value + "," + color2.value + ")";
 	css.textContent  = body.style.background + ";" ;
